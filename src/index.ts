a8d0074c720c353789f995277af8cec341893013
jardenna <hbjornum@gmail.com>
2023-08-05 12:25:58 +0200

    removed unused code

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,5 @@ function greetName(name = 'Helle') {
   }
 }
 
-const test = [1, 2, 3];
-
-const test1 = 456;
-console.log(test, test1);
-
 greetName(Math.random() > 0.5 ? 'name' : 'Test');
 export const hello = 'World is great!';
