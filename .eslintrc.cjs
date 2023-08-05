f44e0b7ef39887c47cf6886557cabf0b674bfb94
jardenna <hbjornum@gmail.com>
2023-08-05 17:22:57 +0200

    added vite

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,11 @@
 module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react-hooks/recommended',
   ],
   reportUnusedDisableDirectives: true,
   parser: '@typescript-eslint/parser',
@@ -10,10 +13,13 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['react-refresh'],
   rules: {
     eqeqeq: 'error',
     'no-console': ['warn', { allow: ['info', 'error'] }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-  root: true,
 };
