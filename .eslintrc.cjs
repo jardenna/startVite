fae9e65712db9a01deaef67f927ad68ee5298e5e
jardenna <hbjornum@gmail.com>
2023-08-05 10:52:17 +0200

    eslinting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,15 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  reportUnusedDisableDirectives: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['@typescript-eslint'],
   root: true,
 };
