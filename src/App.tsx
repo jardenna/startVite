3db0863ae88a0c9299727d6b9b4fc9d73c7bb262
jardenna <hbjornum@gmail.com>
2023-08-06 09:27:06 +0200

    scss + fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1>Project setup</h1>;
+  return (
+    <>
+      <h1>Project setup</h1>
+      <h2>Project setup</h2>
+      <p> This is a basic setup</p>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './scss/index.scss';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(container);
