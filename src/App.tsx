592eecdfcc3df969709e83e1e7fe6c0eab77166e
jardenna <hbjornum@gmail.com>
2023-08-06 11:20:16 +0200

    Colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 function App() {
   return (
-    <>
+    <section className="container">
       <h1>Project setup</h1>
-      <h2>Project setup</h2>
-      <p> This is a basic setup</p>
-    </>
+      <h2>Colors</h2>
+      <p> Reusable Scss colors</p>
+      <article className="grid grid-col-6">
+        <div className="box box1" />
+        <div className="box box2" />
+        <div className="box box3" />
+        <div className="box box4" />
+        <div className="box box5" />
+        <div className="box box6" />
+        <div className="box box11" />
+        <div className="box box12" />
+        <div className="box box13" />
+        <div className="box box14" />
+        <div className="box box15" />
+        <div className="box box16" />
+      </article>
+    </section>
   );
 }
 
