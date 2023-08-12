e735c5f7204cf012dcb4a9d58e34ec734cf2e148
jardenna <hbjornum@gmail.com>
2023-08-12 15:39:00 +0200

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Colors from './pages/Colors';
+import Colors1 from './pages/Color1';
 
 function App() {
   return (
-    <section className="container">
-      <Colors />
+    <section className="main-container">
+      <header className="header">
+        <article className="container">Header</article>
+      </header>
+      <div className="main">
+        <main className="container">
+          <Colors1 />
+        </main>
+      </div>
+      <footer>Footer</footer>
     </section>
   );
 }
