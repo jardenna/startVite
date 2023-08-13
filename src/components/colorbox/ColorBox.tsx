3610c6fd94db896dab27f16877d3ab222248c21a
jardenna <hbjornum@gmail.com>
2023-08-13 19:38:46 +0200

    color

--- src/components/colorbox/ColorBox.tsx
+++ src/components/colorbox/ColorBox.tsx
@@ -6,10 +6,10 @@ interface ColorBoxProps {
   className: string;
 }
 const ColorBox: FC<ColorBoxProps> = ({ title, className }) => (
-  <section className={`grid ${className}`}>
+  <section className={`grid  ${className}`}>
     <h3>{title} color 10% - 80%</h3>
     <div className={`box box1 ${className}`} />
-    <div className="grid grid-col-5">
+    <div className="grid grid-col-4 ">
       <div className="box box2" />
       <div className="box box3" />
       <div className="box box4" />
--- src/pages/ColorPage.tsx
+++ src/pages/ColorPage.tsx
@@ -5,10 +5,15 @@ const ColorPage: FC = () => (
   <>
     <h1>Colors</h1>
     <h2> Reusable Scss colors</h2>
-    <div className="grid grid-col-3">
+    <div className="grid grid-col-2 color-box-container">
       <ColorBox title="Primary" className="primary" />
       <ColorBox title="Secondary" className="secondary" />
+      <ColorBox title="Tertiary" className="tertiary" />
       <ColorBox title="Gray" className="gray" />
+      <ColorBox title="Danger" className="danger" />
+      <ColorBox title="Warning" className="warning" />
+      <ColorBox title="Info" className="info" />
+      <ColorBox title="Success" className="success" />
     </div>
   </>
 );
