c59f60ea7372a9b806a64e0d6b703608bb035e55
jardenna <hbjornum@gmail.com>
2023-08-13 20:36:26 +0200

    danger +

--- src/components/colorbox/ColorBox.tsx
+++ src/components/colorbox/ColorBox.tsx
@@ -4,20 +4,25 @@ import './_colorbox.scss';
 interface ColorBoxProps {
   title: string;
   className: string;
+  boxLength?: number;
 }
-const ColorBox: FC<ColorBoxProps> = ({ title, className }) => (
+const ColorBox: FC<ColorBoxProps> = ({ title, className, boxLength }) => (
   <section className={`grid  ${className}`}>
-    <h3>{title} color 10% - 80%</h3>
+    <h3>{title} color </h3>
     <div className={`box box1 ${className}`} />
     <div className="grid grid-col-4 ">
       <div className="box box2" />
       <div className="box box3" />
       <div className="box box4" />
       <div className="box box5" />
-      <div className="box box6" />
-      <div className="box box7" />
-      <div className="box box8" />
-      <div className="box box9" />
+      {!boxLength && (
+        <>
+          <div className="box box6" />
+          <div className="box box7" />
+          <div className="box box8" />
+          <div className="box box9" />
+        </>
+      )}
     </div>
   </section>
 );
