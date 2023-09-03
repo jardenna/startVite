323b64b12d2c1a30fae6119e66d40b1fc3d47773
jardenna <hbjornum@gmail.com>
2023-09-03 12:30:28 +0200

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,9 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:react/recommended',
     'airbnb',
     'airbnb-typescript',
   ],
