f28a3869e7848706b8d00fd1d0b994fa37e30058
jardenna <hbjornum@gmail.com>
2023-09-03 12:18:54 +0200

    postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-//
+
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+  },
   build: {
     outDir: './build',
   },
