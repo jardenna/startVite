577b4fbb1d5ea38ad069e52f51652dcee872bfd2
jardenna <hbjornum@gmail.com>
2024-07-14 12:29:32 +0200

    added footer and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react';
 
 const App: FC = () => (
-  <main>
-    <h1>App</h1>
-  </main>
+  <article className="main-container">
+    <header>
+      <p>Header</p>
+    </header>
+    <main>
+      <p>Main</p>
+    </main>
+    <footer>
+      <p>Footer</p>
+    </footer>
+  </article>
 );
 
 export default App;
